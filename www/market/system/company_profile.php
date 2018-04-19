77c47d3c6dfbafcabb2b7319d8345df35238a098
Alex_Development_Machine <alfeo.silvestro@live.com>
2018-04-19 13:52:20 +0800

    new line to br replacement logic

--- www/market/system/company_profile.php
+++ www/market/system/company_profile.php
@@ -87,14 +87,16 @@ if (isset($result)){
               <div class="col-md-12">
                   <!--AMK- Added for Tags-->
                 <?php
-                  if($About == null || $About == "")
-                  {
-                    echo "<textarea style='width:100%; resize: none; border: 0px solid white;' readonly='' placeholder='About company. (Your company description here)'></textarea>";
-                  }
-                  else
-                  {
-                    echo "<textarea row='20' style='width:100%; resize: none; border: 0px solid white;' readonly='' placeholder='About company. (Your company description here)'>". $About ."</textarea>";
-                  }
+                  // if($About == null || $About == "")
+                  // {
+                  //   echo "<textarea style='width:100%; resize: none; border: 0px solid white;' readonly='' placeholder='About company. (Your company description here)'></textarea>";
+                  // }
+                  // else
+                  // {
+                  //   echo "<textarea row='20' style='width:100%; resize: none; border: 0px solid white;' readonly='' placeholder='About company. (Your company description here)'>". $About ."</textarea>";
+                  // }
+
+                  echo nl2br($About);
                 ?>
               </div>
             </div>
