937d9b14fff7dd2f61145fbd7b9665e0598c48de
Galles <galles.cs@gmail.com>
2018-06-17 22:17:32 +0800

    fix bug whild gallery downloading

--- www/market/config.php
+++ www/market/config.php
@@ -7,6 +7,6 @@
     $config_db_port = 3306;
 
     $config_gallery_api = "http://35.229.71.137:9909/Galleries/get?page=";
-    $config_gallery_download = "http://35.229.71.137:9909/Galleries/download/";
+    $config_gallery_download = "http://35.229.71.137:9909/Galleries/download?fileid=";
     $config_gallery_search = "http://35.229.71.137:9909/Galleries/get?keyword=<search_key>&page=<page_request>";
 ?>
