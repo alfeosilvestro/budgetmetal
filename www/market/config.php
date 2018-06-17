7ffb2539d95c0a208fd615d95e57b1381241b337
Galles <galles.cs@gmail.com>
2018-06-17 21:24:25 +0800

    change api url

--- www/market/config.php
+++ www/market/config.php
@@ -6,7 +6,7 @@
     $config_db_pass = "vackertech2018";  // mysql password to access the database with.
     $config_db_port = 3306;
 
-    $config_gallery_api = "http://localhost:9909/galleries/get?page=";
-    $config_gallery_download = "http://localhost:9909/galleries/download/";
-    $config_gallery_search = "http://localhost:9909/galleries/get?keyword=<search_key>&page=<page_request>";
+    $config_gallery_api = "http://35.229.71.137:9909/Galleries/get?page=";
+    $config_gallery_download = "http://35.229.71.137:9909/Galleries/download/";
+    $config_gallery_search = "http://35.229.71.137:9909/Galleries/get?keyword=<search_key>&page=<page_request>";
 ?>
